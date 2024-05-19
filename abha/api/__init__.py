ee018f497b9a66bd43bf1544bcb19e8aaa26ab71
Aditya-Kumar-Code <87271653+Aditya-Kumar-Code@users.noreply.github.com>
2024-05-19 21:46:23 +0530

    test

--- abha/api/__init__.py
+++ abha/api/__init__.py
@@ -8,8 +8,9 @@ from abha.api.integrated_programs_api import IntegratedProgramsApi
 from abha.api.login_with_mobile_number_api import LoginWithMobileNumberApi
 from abha.api.profile_api import ProfileApi
 from abha.api.registration_with_aadhaar_api import RegistrationWithAadhaarApi
-from abha.api.registration_with_other_id_documents_api import RegistrationWithOtherIDDocumentsApi
+from abha.api.registration_with_other_id_documents_api import (
+    RegistrationWithOtherIDDocumentsApi,
+)
 from abha.api.registration_with_mobile_number_api import RegistrationWithMobileNumberApi
 from abha.api.search_api import SearchApi
 from abha.api.utility_api import UtilityApi
-
--- examples/auth_with_mobile_token.py
+++ examples/auth_with_mobile_token.py
@@ -1,14 +1,9 @@
 import abha
-from abha.models.auth_with_mobile_txn_and_user_data import AuthWithMobileTxnAndUserData
-from abha.models.jwt_response import JwtResponse
-from abha.rest import ApiException
 from pprint import pprint
-
+import os
 # Defining the host is optional and defaults to http://healthidsbx.abdm.gov.in/api
 # See configuration.py for a list of all supported configuration parameters.
-configuration = abha.Configuration(
-    host = "http://healthidsbx.abdm.gov.in/api"
-)
+configuration = abha.Configuration(host="http://healthidsbx.abdm.gov.in/api")
 
 # The client must configure the authentication and authorization parameters
 # in accordance with the API server security policy.
@@ -16,13 +11,13 @@ configuration = abha.Configuration(
 # satisfies your auth use case.
 
 # Configure API key authorization: Authorization
-configuration.api_key['Authorization'] = os.environ["API_KEY"]
+configuration.api_key["Authorization"] = os.environ["API_KEY"]
 
 # Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
 # configuration.api_key_prefix['Authorization'] = 'Bearer'
 
 # Configure API key authorization: X-HIP-ID
-configuration.api_key['X-HIP-ID'] = os.environ["API_KEY"]
+configuration.api_key["X-HIP-ID"] = os.environ["API_KEY"]
 
 # Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
 # configuration.api_key_prefix['X-HIP-ID'] = 'Bearer'
@@ -31,13 +26,20 @@ configuration.api_key['X-HIP-ID'] = os.environ["API_KEY"]
 with abha.ApiClient(configuration) as api_client:
     # Create an instance of the API class
     api_instance = abha.AuthenticationApi(api_client)
-    auth_request = abha.AuthWithMobileTxnAndUserData() # AuthWithMobileTxnAndUserData | authRequest
-    accept_language = 'en-US' # str |  (optional) (default to 'en-US')
+    auth_request = (
+        abha.AuthWithMobileTxnAndUserData()
+    )  # AuthWithMobileTxnAndUserData | authRequest
+    accept_language = "en-US"  # str |  (optional) (default to 'en-US')
 
     try:
         # Authenticate using verified Mobile Number and user data
-        api_response = api_instance.auth_with_mobile_token_using_post(auth_request, accept_language=accept_language)
+        api_response = api_instance.auth_with_mobile_token_using_post(
+            auth_request, accept_language=accept_language
+        )
         print("The response of AuthenticationApi->auth_with_mobile_token_using_post:\n")
         pprint(api_response)
     except Exception as e:
-        print("Exception when calling AuthenticationApi->auth_with_mobile_token_using_post: %s\n" % e)
+        print(
+            "Exception when calling AuthenticationApi->auth_with_mobile_token_using_post: %s\n"
+            % e
+        )
